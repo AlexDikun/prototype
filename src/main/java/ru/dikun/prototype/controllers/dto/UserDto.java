c43922a43de95df4c81177e47c75a64f4a970cc2
AlexDikun <loban.sasha.97@mail.ru>
2024-09-22 02:07:49 +0300

    added UserDto

---
+++ src/main/java/ru/dikun/prototype/controllers/dto/UserDto.java
@@ -0,0 +1,14 @@
+package ru.dikun.prototype.controllers.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Long id;
+
+    private String login;
+
+    private String password;
+    
+}
