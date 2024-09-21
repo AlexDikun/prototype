af11458731c09d7d4dc4b1b07cb2767e4d71ef9c
AlexDikun <loban.sasha.97@mail.ru>
2024-09-22 02:02:58 +0300

    расставляем иерархию ролей и подчиняем их конфигурации Sequrity

---
+++ src/main/java/ru/dikun/prototype/config/SequrityConfig.java
@@ -0,0 +1,47 @@
+package ru.dikun.prototype.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.access.expression.SecurityExpressionHandler; 
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+
+@EnableWebSecurity
+@Configuration
+public class SequrityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .securityContext((securityContext) -> securityContext.requireExplicitSave(true))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/roleHierarchy")
+                .hasRole("STAFF"));
+
+        return http.build();
+    }
+
+    @Bean
+    public SecurityExpressionHandler<FilterInvocation> customWebSecurityExpressionHandler() {
+        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy());
+        return expressionHandler;
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_ADMIN > ROLE_MODER \n ROLE_MODER > ROLE_STUFF";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
+    }
+    
+}
