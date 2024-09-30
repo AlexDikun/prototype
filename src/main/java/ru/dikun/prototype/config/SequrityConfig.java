f9af42a64feeac1cffa00c86ee1de52dd02792ab
AlexDikun <loban.sasha.97@mail.ru>
2024-09-30 20:58:12 +0300

    syntax fix

--- src/main/java/ru/dikun/prototype/config/SequrityConfig.java
+++ src/main/java/ru/dikun/prototype/config/SequrityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.http.HttpMethod;
 
-@EnableWebSecurity // только для servlet app, нужно закомментировать
+@EnableWebSecurity
 @Configuration
 public class SequrityConfig {
     
