affc7bb67e73df74d4e8a25d6863036189acd5dc
AlexDikun <loban.sasha.97@mail.ru>
2024-10-15 18:30:28 +0300

    переопределили базовую http-аутентификацию

---
+++ src/main/java/ru/dikun/prototype/config/ApplicationConfig.java
@@ -0,0 +1,48 @@
+package ru.dikun.prototype.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import ru.dikun.prototype.sevrice.UserService;
+
+@Configuration
+public class ApplicationConfig {
+
+    UserService userService;
+
+    public ApplicationConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userService.loadUserByUsername(username);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+}
