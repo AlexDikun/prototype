d71cc1b7d9140303f6dd3ed2401eaad30b7cd025
AlexDikun <loban.sasha.97@mail.ru>
2025-03-02 02:41:56 +0300

    рефакторинг

--- src/test/java/ru/dikun/prototype/repos/RoleRepoTests.java
+++ src/test/java/ru/dikun/prototype/repos/RoleRepoTests.java
@@ -10,10 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import ru.dikun.prototype.domain.RoleEntity;
 
